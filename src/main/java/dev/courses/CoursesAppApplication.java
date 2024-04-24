44c42f37180f1b6606cbf0908fbd2552eccb613a
SudarshanShah <sudarshanshah911@gmail.com>
2024-04-24 10:16:08 +0530

    Initial commit

---
+++ src/main/java/dev/courses/CoursesAppApplication.java
@@ -0,0 +1,13 @@
+package dev.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesAppApplication.class, args);
+	}
+
+}
